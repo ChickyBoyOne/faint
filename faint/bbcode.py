9911862d4297d4bdb5a2fc03b644953f09d3e571
WrenFing <84818342+WrenFing@users.noreply.github.com>
2021-05-16 23:31:09 -0500

    Implement BBCode links and add comments

--- faint/bbcode.py
+++ faint/bbcode.py
@@ -33,9 +33,11 @@ def to_bbcode(tag: Tag, descendant: bool = False) -> str:
         # Newlines
         elif child.name == 'br':
             bbcode += '\n'
-        # All other tags
+        # All other clearly identified tags
         elif (bbcode_tag_classes := [c for c in child['class'] if 'bbcode_' in c]) \
                 and len(bbcode_tag_parts := bbcode_tag_classes[0].split("_")) == 2:
+            # Handle special cases
+            # Quote has optional parameter
             if (bbcode_tag := bbcode_tag_parts[-1]) == 'quote':
                 contents = to_bbcode(child, descendant=True)
 
@@ -43,11 +45,37 @@ def to_bbcode(tag: Tag, descendant: bool = False) -> str:
                     bbcode += f'[quote={author.get_text().split()[0]}]{contents}[/quote]'
                 else:
                     bbcode += f'[quote]{contents}[/quote]'
+            # Horizontal line becomes text instead of tag
             elif bbcode_tag == 'hr':
                 bbcode += '-----'
             else:
                 bbcode += f'[{bbcode_tag}]{to_bbcode(child, descendant=True)}[/{bbcode_tag}]'
+        # Color tag
         elif child.name == 'span' and (style := child.get('style', '')):
             bbcode += f'[color={style.split()[-1][:-1]}]{to_bbcode(child, descendant=True)}[/color]'
+        # Various link-based tags
+        elif child.name == 'a':
+            # Explicit URL tags
+            if 'named_url' in (cls := child["class"]):
+                bbcode += f'[url={child["href"]}]{to_bbcode(child, descendant=True)}[/url]'
+            # Implicit email links
+            elif 'email' in cls:
+                bbcode += child.get_text().replace('[at]', '@')
+            # Implicit HTTP links
+            elif 'auto_link' in cls:
+                bbcode += child['href'][child['href'].find(child.get_text().partition(".....")[0]):]
+            # Text user links
+            elif 'linkusername' in cls:
+                bbcode += f':link{child.get_text()}:'
+            # Icon user links
+            else:
+                username = child.img['alt']
+
+                # With text
+                if child.get_text():
+                    bbcode += f':icon{username}:'
+                # Without text
+                else:
+                    bbcode += f':{username}icon:'
     
     return bbcode
