3d716ab3a6000d2cd974c7489ddf3b1091a099f8
WrenFing <84818342+WrenFing@users.noreply.github.com>
2021-05-16 19:34:13 -0500

    Pass newline BBCode test

--- faint/bbcode.py
+++ faint/bbcode.py
@@ -1,6 +1,22 @@
-from bs4.element import Tag
+from bs4.element import NavigableString, Tag
 
-# TODO: Implement BBCode
-# https://www.furaffinity.net/journal/833448/
 def to_bbcode(tag: Tag) -> str:
-    return str(tag)
+    bbcode = ''
+
+    for i, child in enumerate(tag.contents):
+        if type(child) == NavigableString:
+            child = child.replace('\n', '')
+
+            if i == 0:
+                bbcode += child[16:]
+            elif i == len(tag.contents) - 1:
+                bbcode += child[:-12]
+            else:
+                bbcode += child
+            
+            continue
+        
+        if child.name == 'br':
+            bbcode += '\n'
+    
+    return bbcode
