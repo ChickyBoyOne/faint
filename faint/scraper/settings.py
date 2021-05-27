482c7d0b1972e4e00476d4f378ae9d40ac098f50
WrenFing <84818342+WrenFing@users.noreply.github.com>
2021-05-27 00:33:13 -0500

    Ignore robots.txt (sorry, I gotta scrape settings)

--- faint/scraper/settings.py
+++ faint/scraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'faint.scraper.spiders'
 USER_AGENT = 'faint/0.1 (+https://furaffinity.net/user/WrenFing/)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
